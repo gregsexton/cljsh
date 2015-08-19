1f9eee3e3438df32e59a067a5031583c6a7a3b50
Greg Sexton <gregsexton@gmail.com>
2015-08-19 20:46:23 +0100

    Get basic cmd and pipes working

--- src/shell/core.clj
+++ src/shell/core.clj
@@ -1,5 +1,11 @@
 (ns shell.core
-  (:gen-class))
+  (:gen-class)
+  (:require [shell.proc :refer :all]))
+
+;;; TODO: what should this take and return?
+;;; TODO: stderr
+(defn eval [form]
+  (-> (form) :out slurp))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- src/shell/proc.clj
+++ src/shell/proc.clj
@@ -1,10 +1,53 @@
-(ns shell.proc)
+(ns shell.proc
+  (:require [clojure.java.io :as io]
+            [me.raynes.conch.low-level :as conch])
+  (:import [java.io InputStream]))
 
-(defn cmd [])
+(defprotocol Pipeable
+  (pipe [this out-stream]))
 
-(defmacro |-> [& cmds])
+(extend-type InputStream
+  Pipeable
+  (pipe [this out-stream]
+    (io/copy this out-stream)
+    (.close out-stream)))
 
+(extend-type String
+  Pipeable
+  (pipe [this out-stream]
+    ;; TODO: encoding
+    (io/copy (.getBytes this) out-stream)
+    (.close out-stream)))
+
+(extend-type nil
+  Pipeable
+  (pipe [_ out-stream]
+    ;; TODO: should this close? how would you type and C-d?
+    (.close out-stream)))
+
+(defn cmd* [program & args]
+  (fn this
+    ([] (this {:out nil}))
+    ([input]
+     (let [p (apply conch/proc program args)]
+       (pipe (:out input) (:in p))
+       p))))
+
+(defmacro cmd [program & args]
+  (letfn [(normalize [arg]
+            (cond (symbol? arg) (str arg)
+                  (number? arg) (str arg)
+                  :else arg))]
+    `(cmd* (str '~program) ~@(map normalize args))))
+
+;;; TODO: need to join together the error streams, should do this in cmd
+(defn | [& cmds]
+  (apply comp (reverse cmds)))
+
+;;; TODO: these conflict...
 (defn < [])
+
+(defn << [str] {:out str})
 
 (defn > [])
 
